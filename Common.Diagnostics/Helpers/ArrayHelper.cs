049bb19bb4159f0d4482685c76c6409e377e1c78
Dario Airoldi <darioa@microsoft.com>
2024-02-07 21:43:49 +0100

    .

--- Common.Diagnostics/Helpers/ArrayHelper.cs
+++ Common.Diagnostics/Helpers/ArrayHelper.cs
@@ -11,7 +11,7 @@ namespace Common
         public static T[] SubArray<T>(this T[] data, int index, int length)
         {
             T[] result = new T[length];
-            Array.Copy(data, index, result, 0, length); 
+            Array.Copy(data, index, result, 0, length);
             return result;
         }
         public static byte[] SwapBytesInWord(byte[] ba)
