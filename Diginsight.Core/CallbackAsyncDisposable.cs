a9f9801af985dccd4cc036787dff56499cf095b9
DARIO AIROLDI <dario.airoldi@it.abb.com>
2024-03-19 00:48:38 -0700

    v3.0.0-alpha.140

--- Diginsight.Core/CallbackAsyncDisposable.cs
+++ Diginsight.Core/CallbackAsyncDisposable.cs
@@ -1,6 +1,7 @@
 ﻿#if NET6_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
 namespace Diginsight;
-
+ 
+ 
 public sealed class CallbackAsyncDisposable : IAsyncDisposable
 {
     private readonly Func<ValueTask> actionAsync;
