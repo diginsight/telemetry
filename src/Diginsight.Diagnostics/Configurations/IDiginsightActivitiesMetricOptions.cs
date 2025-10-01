4fe74642fdeddd8cdefe8fc1baf8d68a47a583d5
Dario Airoldi <dario.airoldi@it.abb.com>
2025-10-01 19:22:05 +0200

    fix

---
+++ src/Diginsight.Diagnostics/Configurations/IDiginsightActivitiesMetricOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Diginsight.Diagnostics;
+
+public interface IDiginsightActivitiesMetricOptions
+{
+    bool Record { get; }
+    string MeterName { get; }
+    string MetricName { get; }
+    string? MetricDescription { get; }
+}
