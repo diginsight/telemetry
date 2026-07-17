d79116865156c32429f08076ac9b3e32c4db838f
Filippo Mineo <filippo.mineo@outlook.com>
2026-07-17 18:13:53 +0200

    Improved EarlyLoggingManager

--- src/Diginsight.Diagnostics/EarlyLoggingManager.cs
+++ src/Diginsight.Diagnostics/EarlyLoggingManager.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Diginsight.Diagnostics;
 
@@ -10,6 +11,8 @@ public class EarlyLoggingManager : IDisposable
     private readonly DeferredOperationRegistry operationRegistry;
     private readonly DeferredLoggerFactory loggerFactory;
     private readonly DeferredActivityLifecycleLogEmitter logEmitter;
+
+    protected IServiceCollection? services;
 
     private ILoggerFactory? emergencyLoggerFactory;
     private ActivityLifecycleLogEmitter? emergencyLogEmitter;
@@ -24,23 +27,51 @@ public class EarlyLoggingManager : IDisposable
         logEmitter = new DeferredActivityLifecycleLogEmitter(operationRegistry, shouldListenTo, timeProvider, GetEmergencyLogEmitter);
     }
 
-    protected ILoggerFactory GetEmergencyLoggerFactory() => emergencyLoggerFactory ??= MakeEmergencyLoggerFactory();
+    protected ILoggerFactory GetEmergencyLoggerFactory()
+    {
+        try
+        {
+            return emergencyLoggerFactory ??= MakeEmergencyLoggerFactory();
+        }
+        catch (Exception)
+        {
+            return NullLoggerFactory.Instance;
+        }
+    }
 
-    protected virtual ILoggerFactory MakeEmergencyLoggerFactory() => NullLoggerFactory.Instance;
+    protected virtual ILoggerFactory MakeEmergencyLoggerFactory()
+    {
+        return services?.BuildServiceProvider().GetRequiredService<ILoggerFactory>() ?? NullLoggerFactory.Instance;
+    }
 
-    protected ActivityLifecycleLogEmitter GetEmergencyLogEmitter() => emergencyLogEmitter ??= MakeEmergencyLogEmitter();
+    protected ActivityLifecycleLogEmitter GetEmergencyLogEmitter()
+    {
+        try
+        {
+            return emergencyLogEmitter ??= MakeEmergencyLogEmitter();
+        }
+        catch (Exception)
+        {
+            return ActivityLifecycleLogEmitter.Noop;
+        }
+    }
 
-    protected virtual ActivityLifecycleLogEmitter MakeEmergencyLogEmitter() => ActivityLifecycleLogEmitter.Noop;
+    protected virtual ActivityLifecycleLogEmitter MakeEmergencyLogEmitter()
+    {
+        return services?.BuildServiceProvider().GetRequiredService<ActivityLifecycleLogEmitter>() ?? ActivityLifecycleLogEmitter.Noop;
+    }
 
-    public void AttachTo(IServiceCollection services)
+    public void AttachTo([SuppressMessage("ReSharper", "ParameterHidesMember")] IServiceCollection services)
     {
         services.FlushOnCreateServiceProvider(loggerFactory);
         services.FlushOnCreateServiceProvider(logEmitter);
 
+        this.services = services;
+
         AdditionalAttachTo(services);
     }
 
-    protected virtual void AdditionalAttachTo(IServiceCollection services) { }
+    protected virtual void AdditionalAttachTo([SuppressMessage("ReSharper", "ParameterHidesMember")] IServiceCollection services) { }
 
     public void Dispose()
     {
